d97c1b15bb1da8356079e7117756c0730443233b
Ruslan Gavinov <rg@wishfinance.com>
2022-12-04 21:59:18 +0300

    поправил обработку ошибок

--- backend/app/main.py
+++ backend/app/main.py
@@ -34,42 +34,45 @@ def get_data(lang, word_items):
     dictionaries = list()
     dic_list = psyco.get_dictionaries(query=query_dic, language=lang)
     words = word_items.split(',')
-    for d in dic_list:
-        d['results'] = dict()
-        precise = list()
-        stems = list()
-        examples = list()
-        dic_id = d['dic_id']
-        for w in words:
-            # обрабатываем перевод слова
-            translation = psyco.get_word(query=query_word, word=w, dic=dic_id)
-            if translation:
-                translation['isResult'] = True
-            else:
-                translation = dict(word=w, isResult=False, transcription=None, translation=None)
-            precise.append(translation)
+    if dic_list:
+        for d in dic_list:
+            d['results'] = dict()
+            precise = list()
+            stems = list()
+            examples = list()
+            dic_id = d['dic_id']
+            if words:
+                for w in words:
+                    w = w.strip()
+                    # обрабатываем перевод слова
+                    translation = psyco.get_word(query=query_word, word=w, dic=dic_id)
+                    if translation:
+                        translation['isResult'] = True
+                    else:
+                        translation = dict(word=w, isResult=False, transcription=None, translation=None)
+                    precise.append(translation)
 
-            # обрабатываем стемы слова
-            matches_list = psyco.get_item(query=query_stem, word=w, dic=dic_id)
-            if matches_list:
-                m = [x for x in matches_list if x['word'] != w]
-                s = dict(word=w, isResult=True, matches=m)
-            else:
-                s = dict(word=w, isResult=False, matches=matches_list)
-            stems.append(s)
+                    # обрабатываем стемы слова
+                    matches_list = psyco.get_item(query=query_stem, word=w, dic=dic_id)
+                    if matches_list:
+                        m = [x for x in matches_list if x['word'] != w]
+                        s = dict(word=w, isResult=True, matches=m)
+                    else:
+                        s = dict(word=w, isResult=False, matches=matches_list)
+                    stems.append(s)
 
-            # ищем в примерах
-            example = psyco.get_item(query=query_examples, word=w, dic=dic_id)
-            sentences = list(map(lambda x: x["examples"], example))
-            if sentences:
-                e = dict(word=w, isResult=True, sentences=sentences)
-            else:
-                e = dict(word=w, isResult=False, sentences=None)
-            examples.append(e)
+                    # ищем в примерах
+                    example = psyco.get_item(query=query_examples, word=w, dic=dic_id)
+                    sentences = list(map(lambda x: x["examples"], example))
+                    if sentences:
+                        e = dict(word=w, isResult=True, sentences=sentences)
+                    else:
+                        e = dict(word=w, isResult=False, sentences=None)
+                    examples.append(e)
 
-        d['results']['precise'] = precise
-        d['results']['stems'] = stems
-        d['results']['examples'] = examples
-        dictionaries.append(d)
+                d['results']['precise'] = precise
+                d['results']['stems'] = stems
+                d['results']['examples'] = examples
+                dictionaries.append(d)
     return dictionaries
 
